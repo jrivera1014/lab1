1dadf455c4f300575075a4e56f7324369f74cd92
jrivera1014 <143525032+jrivera1014@users.noreply.github.com>
2023-09-05 07:15:15 -0700

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -265,12 +265,12 @@ void render()
 	//
 	glClear(GL_COLOR_BUFFER_BIT);
 	//Draw box.
-	glPushMatrix();
-	if(g.xres < 350)
+	glPushMatrix()
+	if(g.xres < 400)
 	{
 		glColor3f(1.0, 0.0, 0.0);
 	}
-	else if(g.xres > 450)
+	else if(g.xres >= 400)
 	{
 		glColor3f(0.0, 0.0, 1.0);
 	}
